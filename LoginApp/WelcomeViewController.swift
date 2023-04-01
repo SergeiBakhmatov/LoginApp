0bd2e9b1dac958153da1e07c9e23eb2f0a9e21fc
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-04-01 13:00:57 +0500

    Rename ViewController. Add Welcome ViewController

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomViewController.swift
+//  LoginApp
+//
+//  Created by Sergei Bakhmatov on 01.04.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
