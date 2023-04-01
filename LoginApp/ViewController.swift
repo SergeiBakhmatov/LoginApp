44239deba25530249fc09fac34a9fd9a715c582a
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-04-01 11:42:11 +0500

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Sergei Bakhmatov on 01.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
